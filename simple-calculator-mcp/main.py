0be00bd18c7bafbacbfd6920ccd144a4ff1f58ca
Nikhil Popli <nikhil@truefoundry.com>
2025-07-03 15:35:21 +0530

    nit

--- simple-calculator-mcp/main.py
+++ simple-calculator-mcp/main.py
@@ -30,4 +30,4 @@ def square(a: int) -> int:
 
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000, path="/mcp")
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000, path="/mcp")
